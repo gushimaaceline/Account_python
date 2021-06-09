5ae1d91d08960eddfca54f9fef875597b3555868
gushimaaceline <celinegushimaa@gmail.com>
2021-06-09 21:51:55 +0300

    update my code

--- bank.py
+++ bank.py
@@ -1,5 +1,11 @@
+
+from datetime import datetime, time
+
+
 class Account:
     def __init__(self,name,phone):
+            
+            self.transactions=[]
             self.name=name
             self.phone=phone
             self.transaction_fee=500
@@ -12,8 +18,22 @@ class Account:
 
         else:
              self.balance+=amount
+             transaction={
+             "amount":amount,
+             "balance":self.balance,
+             "time":datetime.now(),
+             "narration":"Deposit"
+                }
+             self.transactions.append(transaction)
              return f"Hello {self.name} you have deposited {amount} your new balance is {self.balance}"
-
+    def get_statement(self):
+        for transaction in self.transactions:
+            narration=transaction["narration"]
+            amount=transaction["amount"]
+            balance=transaction["balance"]
+            time=transaction["time"]
+            date=time.strftime("%D")
+            print (f"{date}.. {narration}..{amount}..Balance {balance}")
     def withdraw(self,amount):
         total_amount=amount+self.transaction_fee
         if amount<=0:
@@ -21,8 +41,16 @@ class Account:
         elif self.balance<total_amount:
             return "You can have insufficient balance"
         else:
-             self.balance-=total_amount
-             return f"You have successfully withdrawn {amount} your balance is {self.balance}"
+            self.balance-=total_amount
+            transaction={
+                "amount":amount,
+                "balance":self.balance,
+                "time":datetime.now(),
+                "narration":"withdraw"
+                }
+                
+            self.transactions.append(transaction)
+            return f"You have successfully withdrawn {amount} your balance is {self.balance}"
              
     def borrow(self,amount):
        
@@ -37,5 +65,37 @@ class Account:
             loan_fee=amount*0.05
             self.loan=amount+loan_fee
             self.balance+=amount
+            self.loan+=1
+            self.balance+=amount
+            transaction={
+                "amount":amount,
+                "balance":self.balance,
+                "time":datetime.now(),
+                "narration":"Loan"
+                }
+            self.transactions.append(transaction)
             return f"hello {self.name}.You have taken a loan of {amount},your new balance is {self.balance} and your loan balance is {self.loan}"
+
+
+    def repay_loan(self,amount):
+        
+        
+        if amount<=0:
+            return "Please deposit a positive amount"
+        
+        elif amount<self.loan:
+            outstanging_loan= self.loan-amount
+            return f"You have paid {amount} on your loan and your outstand loan is {outstanging_loan}"
+        
+        else:
+            difference=amount-self.loan
+            self.balance+=difference
+            transaction={
+                "amount":amount,
+                "balance":self.balance,
+                "time":datetime.now(),
+                "narration":"Loan"
+                }
+            self.transactions.append(transaction)
+            return f"You have fully paid your loan and your balance is {self.balance}"        
              
